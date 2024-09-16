7624b0787f74728a7d4bf8f9724106ba7f93ee6b
Bastian Schaffer <bastianschaffer.it@gmail.com>
2024-09-16 10:28:38 +0200

    Add Write Permission Check
    Fixes: #31

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -e
 
+if [ ! -w /app/output ]; then
+    echo "Missing writing permissions on output directory" >&2
+    exit 1
+fi
+
 today=$(date +"%Y-%m-%d_%H-%M-%S")
 measureName="$(jq -c --raw-output '.name' /app/measure.json)"
 outputDir="$today-$measureName"
