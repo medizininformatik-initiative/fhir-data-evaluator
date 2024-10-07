3551d1f6cdd89e8fd012a4926e2e5ca0da45f1ad
Julian Gründner <juliangruendner@googlemail.com>
2024-10-07 09:27:47 +0200

    Add support for self signed certificates

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -e
 
+TRUSTSTORE_FILE="/app/truststore/self-signed-truststore.jks"
+TRUSTSTORE_PASS=${TRUSTSTORE_PASS:-changeit}
+KEY_PASS=${KEY_PASS:-changeit}
+
 if [ ! -w /app/output ]; then
     echo "Missing writing permissions on output directory" >&2
     exit 1
@@ -11,7 +15,35 @@ outputDir="$today-$measureName"
 mkdir -p /app/output/"$outputDir"
 cp /app/measure.json /app/output/"$outputDir"/measure.json
 
-java -jar fhir-data-evaluator.jar "$outputDir"
+shopt -s nullglob
+IFS=$'\n'
+ca_files=(certs/*.pem)
+
+if [ ! "${#ca_files[@]}" -eq 0 ]; then
+
+    echo "# At least one CA file with extension *.pem found in certs folder -> starting flare with own CAs"
+
+    if [[ -f "$TRUSTSTORE_FILE" ]]; then
+          echo "## Truststore already exists -> resetting truststore"
+          rm "$TRUSTSTORE_FILE"
+    fi
+
+    keytool -genkey -alias self-signed-truststore -keyalg RSA -keystore "$TRUSTSTORE_FILE" -storepass "$TRUSTSTORE_PASS" -keypass "$KEY_PASS" -dname "CN=self-signed,OU=self-signed,O=self-signed,L=self-signed,S=self-signed,C=TE"
+    keytool -delete -alias self-signed-truststore -keystore "$TRUSTSTORE_FILE" -storepass "$TRUSTSTORE_PASS" -noprompt
+
+    for filename in "${ca_files[@]}"; do
+
+      echo "### ADDING CERT: $filename"
+      keytool -delete -alias "$filename" -keystore "$TRUSTSTORE_FILE" -storepass "$TRUSTSTORE_PASS" -noprompt > /dev/null 2>&1
+      keytool -importcert -alias "$filename" -file "$filename" -keystore "$TRUSTSTORE_FILE" -storepass "$TRUSTSTORE_PASS" -noprompt
+
+    done
+
+    java -Djavax.net.ssl.trustStore="$TRUSTSTORE_FILE" -Djavax.net.ssl.trustStorePassword="$TRUSTSTORE_PASS" -jar fhir-data-evaluator.jar "$outputDir"
+else
+    echo "# No CA *.pem cert files found in /app/certs -> starting flare without own CAs"
+    java -jar fhir-data-evaluator.jar "$outputDir"
+fi
 
 if [ "${CONVERT_TO_CSV}" = true ]; then
   bash /app/csv-converter.sh /app/output/"$outputDir"
